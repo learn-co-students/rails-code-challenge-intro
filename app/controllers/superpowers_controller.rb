862802966afaaa8a2403a66790396d1304cc5480
cs-j <cj@mail.co.de>
2018-02-07 16:40:47 -0800

    completed rails challenge

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -2,4 +2,8 @@ class SuperpowersController < ApplicationController
   def index
     @superpowers = Superpower.all
   end
+
+  def show
+    @superpower = Superpower.find_by(id: params[:id])
+  end
 end
